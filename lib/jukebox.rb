fb2e09800d4be7d801998a578d7511f20b443158
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-25 23:32:17 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,11 +21,13 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.chomp
-  songs.each do |song|
-    if song == user_input
-      puts "Playing #{song}"
-    #else
-    #  puts "Invalid input, please try again"
+  if songs.include?(user_input) || user_input <= songs.length
+    songs.each do |song|
+      if song == user_input
+        puts "Playing #{song}"
+      #else
+      #  puts "Invalid input, please try again"
+      end
     end
   end
 end
