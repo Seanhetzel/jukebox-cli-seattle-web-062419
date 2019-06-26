6c74c950eb755ae756dba2d267afe06dcc28b25e
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-26 00:39:31 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -9,7 +9,7 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
-require "pry"
+
 def help
   puts "  I accept the following commands:"
   puts "- help : displays this help message"
